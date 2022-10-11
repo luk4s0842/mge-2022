b885a4035157ca6e3bb177e352f677fa5a4784c0
Andri Joos <andri@joos.io>
2022-10-11 20:00:27 +0200

    add TMApiWrapper

---
+++ MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/TMApiWrapper.kt
@@ -0,0 +1,40 @@
+package com.habeggerdomeisenjoos.mge_2022
+
+public class TMApiWrapper { // static class or singleton
+    companion object {
+        private const val API_VERSION: String = "v2"
+
+        //packages
+        private const val API_PACKAGE_DISCOVERY: String = "discovery"
+
+        // API params
+        private const val API_PARAM_KEYWORD: String = "keyword"
+        private const val API_PARAM_TICKETSOURCE: String = "source"
+        private const val API_PARAM_COUNTRYCODE: String = "countryCode"
+        private const val API_PARAM_CLASSIFICATIONNAME: String = "classificationName"
+
+        // possible values for API param classificationName
+        private const val API_CLASSIFICATIONNAME_VALUE_MUSIC: String = "music"
+        private const val API_CLASSIFICATIONNAME_VALUE_FILM: String = "film"
+    }
+    private val BASE_URL: String = "https://app.ticketmaster.com/$API_PACKAGE_DISCOVERY/$API_VERSION/{resource}.json?apikey=**$API_KEY"
+
+    // ticket sellers
+    enum class ticketSources {
+        ticketmaster,
+        universe,
+        frontGate,
+        ticketmaster_resale,
+    }
+    enum class classificationNames { //not sure if that works, example very bad
+        music,
+        film,
+    }
+
+    private fun createRequestUrl(
+        sources: List<ticketSources>? = null,
+        countryCode: String? = null,
+        classificationName: classificationNames? = null){
+
+    }
+}
