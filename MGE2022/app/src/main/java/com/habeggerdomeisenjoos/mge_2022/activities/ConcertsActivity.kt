060af8d5fb80fa94e92ef4da57d5f0921a23436d
kaj.habegger <kaj.habegger@ost.ch>
2022-10-28 10:07:37 +0200

    Modified folder structure and renamed MainActivity to ConcertsActivity

--- MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/activities/ConcertsActivity.kt
+++ MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/activities/ConcertsActivity.kt
@@ -1,7 +1,8 @@
-package com.habeggerdomeisenjoos.mge_2022
+package com.habeggerdomeisenjoos.mge_2022.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.habeggerdomeisenjoos.mge_2022.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
