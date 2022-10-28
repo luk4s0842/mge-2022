d229535ae572da1068cdb33437e190616b8ed4f6
lukas.domeisen <lukas.domeisen@ost.ch>
2022-10-28 15:05:22 +0200

    added navigation bar + artists activity + settings activity

--- MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/activities/ArtistsActivity.kt
+++ MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/activities/ArtistsActivity.kt
@@ -1,17 +1,13 @@
 package com.habeggerdomeisenjoos.mge_2022.activities
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentContainerView
-import androidx.fragment.app.FragmentManager
+import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.habeggerdomeisenjoos.mge_2022.R
 
-
-class ConcertsActivity : AppCompatActivity() {
+class ArtistsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_artists)
     }
 }
