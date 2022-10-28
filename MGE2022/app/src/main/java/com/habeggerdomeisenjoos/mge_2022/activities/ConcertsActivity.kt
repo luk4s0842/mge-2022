3e52bca694c974da12a70a52341721b3b8c77236
kaj.habegger <kaj.habegger@ost.ch>
2022-10-28 10:16:35 +0200

    fixup! Activitiy

--- MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/activities/ConcertsActivity.kt
+++ MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/activities/ConcertsActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.habeggerdomeisenjoos.mge_2022.R
 
-class MainActivity : AppCompatActivity() {
+class ConcertsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
