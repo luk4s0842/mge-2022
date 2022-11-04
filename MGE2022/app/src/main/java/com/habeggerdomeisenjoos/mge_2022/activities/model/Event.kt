599a37c247b37e326654875f4e9fc02b97d9def4
lukas.domeisen <lukas.domeisen@ost.ch>
2022-11-04 14:48:11 +0100

    added events list to events activity

---
+++ MGE2022/app/src/main/java/com/habeggerdomeisenjoos/mge_2022/activities/model/Event.kt
@@ -0,0 +1,12 @@
+package com.habeggerdomeisenjoos.mge_2022.activities.model
+
+import java.time.LocalDateTime
+
+class Event(
+    val title: String,
+    val artistName: String,
+    val location: String,
+    val datetime: LocalDateTime,
+    val imageUrl: String) {
+
+}
